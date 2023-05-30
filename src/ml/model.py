c8545067ef5a506cade82f196f15983143ff1e47
Julian Bolivar <jbolivarg@gmail.com>
2023-05-30 09:52:26 -0400

    Style fixes

--- src/ml/model.py
+++ src/ml/model.py
@@ -1,11 +1,26 @@
-from sklearn.metrics import fbeta_score, precision_score, recall_score, confusion_matrix
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.model_selection import GridSearchCV
-import pandas as pd
+"""
+model definition
+
+By: Julian Bolivar
+Version: 1.0.0
+Date:  2023-05-29
+Revision 1.0.0 (2023-05-29): Initial Release
+"""
+
+# System's imports
 import multiprocessing
 import logging
 
-# Optional: implement hyperparameter tuning.
+# Data managments imports
+from sklearn.metrics import fbeta_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
+
+
 def train_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
@@ -40,14 +55,15 @@ def train_model(X_train, y_train):
 
     clf.fit(X_train, y_train)
     logging.info("********* Best parameters found ***********")
-    logging.info("BEST PARAMS: {}".format(clf.best_params_))
+    logging.info(f"BEST PARAMS: {clf.best_params_}")
 
     return clf
 
 
 def compute_model_metrics(y, preds):
     """
-    Validates the trained machine learning model using precision, recall, and F1.
+    Validates the trained machine learning model using precision,
+    recall, and F1.
 
     Inputs
     ------
@@ -86,7 +102,7 @@ def inference(model, X):
     return preds
 
 
-def compute_confusion_matrix(y, preds, labels=None):
+def compute_confusion_matrix(y, preds):
     """
     Compute confusion matrix using the predictions and ground thruth provided
     Inputs
@@ -103,13 +119,12 @@ def compute_confusion_matrix(y, preds, labels=None):
     return cm
 
 
-
 def compute_slices(df, feature, y, preds):
     """
     Compute the performance on slices for a given categorical feature
     a slice corresponds to one value option of the categorical feature analyzed
     ------
-    df: 
+    df:
         test dataframe pre-processed with features as column used for slices
     feature:
         feature on which to perform the slices
@@ -126,17 +141,23 @@ def compute_slices(df, feature, y, preds):
         recall : float
         fbeta : float
     row corresponding to each of the unique values taken by the feature (slice)
-    """    
+    """
     slice_options = df[feature].unique().tolist()
-    perf_df = pd.DataFrame(index=slice_options, 
-                            columns=['feature','n_samples','precision', 'recall', 'fbeta'])
+    perf_df = pd.DataFrame(
+        index=slice_options,
+        columns=[
+            'feature',
+            'n_samples',
+            'precision',
+            'recall',
+            'fbeta'])
     for option in slice_options:
-        slice_mask = df[feature]==option
+        slice_mask = df[feature] == option
 
         slice_y = y[slice_mask]
         slice_preds = preds[slice_mask]
         precision, recall, fbeta = compute_model_metrics(slice_y, slice_preds)
-        
+
         perf_df.at[option, 'feature'] = feature
         perf_df.at[option, 'n_samples'] = len(slice_y)
         perf_df.at[option, 'precision'] = precision
@@ -146,7 +167,7 @@ def compute_slices(df, feature, y, preds):
     # reorder columns in performance dataframe
     perf_df.reset_index(names='feature value', inplace=True)
     colList = list(perf_df.columns)
-    colList[0], colList[1] =  colList[1], colList[0]
+    colList[0], colList[1] = colList[1], colList[0]
     perf_df = perf_df[colList]
 
     return perf_df
--- src/ml/model_train.py
+++ src/ml/model_train.py
@@ -14,11 +14,12 @@ from argparse import ArgumentParser
 import logging as log
 import logging.handlers
 import os
+import sys
 import platform
+import pickle
 
 # ML Imports
 from sklearn.model_selection import train_test_split
-import pickle
 import pandas as pd
 from data import process_data
 from model import train_model, compute_model_metrics, inference, compute_slices
@@ -40,26 +41,26 @@ def build_argparser():
     parser = ArgumentParser(prog="model_trainner",
                             description="Train the model")
     parser.add_argument("-d",
-        "--data", 
-        type=str,
-        help="Data Path",
-        default="../../data/census_clean.csv",
-        required=False
-    )
+                        "--data",
+                        type=str,
+                        help="Data Path",
+                        default="../../data/census_clean.csv",
+                        required=False
+                        )
     parser.add_argument("-m",
-        "--modelpath", 
-        type=str,
-        help="Path where the model is saved",
-        default="../../model",
-        required=False
-    )
+                        "--modelpath",
+                        type=str,
+                        help="Path where the model is saved",
+                        default="../../model",
+                        required=False
+                        )
     parser.add_argument("-s",
-        "--slicefile", 
-        type=str,
-        help="File where the slices test result are saved",
-        default="./slice_output.txt",
-        required=False
-    )
+                        "--slicefile",
+                        type=str,
+                        help="File where the slices test result are saved",
+                        default="./slice_output.txt",
+                        required=False
+                        )
     return parser.parse_args()
 
 
@@ -89,11 +90,11 @@ def main(args):
     data = pd.read_csv(datapath)
 
     # separate train and test slices
-    train, test = train_test_split( data, 
-                                test_size=0.20, 
-                                random_state=10, 
-                                stratify=data['salary']
-                                )
+    train, test = train_test_split(data,
+                                   test_size=0.20,
+                                   random_state=10,
+                                   stratify=data['salary']
+                                   )
 
     cat_features = [
         "workclass",
@@ -123,48 +124,54 @@ def main(args):
         encoder=encoder,
         lb=lb
     )
-    
+
     # check if trained model already exists
     savepath = args.modelpath
     filename = ['trained_model.pkl', 'encoder.pkl', 'labelizer.pkl']
-    
+
     # if saved model exits, load the model from disk
-    if os.path.isfile(os.path.join(savepath,filename[0])):
-            model = pickle.load(open(os.path.join(savepath,filename[0]), 'rb'))
-            encoder = pickle.load(open(os.path.join(savepath,filename[1]), 'rb'))
-            lb = pickle.load(open(os.path.join(savepath,filename[2]), 'rb'))
-    
+    if os.path.isfile(os.path.join(savepath, filename[0])):
+        with open(os.path.join(savepath, filename[0]), 'rb') as file:
+            model = pickle.load(file)
+        with open(os.path.join(savepath, filename[1]), 'rb') as file:
+            encoder = pickle.load(file)
+        with open(os.path.join(savepath, filename[2]), 'rb') as file:
+            lb = pickle.load(file)
+
     # Else Train and save a model.
     else:
         model = train_model(X_train, y_train)
-        # save model  to disk 
-        pickle.dump(model, open(os.path.join(savepath,filename[0]), 'wb'))
-        pickle.dump(encoder, open(os.path.join(savepath,filename[1]), 'wb'))
-        pickle.dump(lb, open(os.path.join(savepath,filename[2]), 'wb'))
+        # save model  to disk
+        with open(os.path.join(savepath, filename[0]), 'wb') as file:
+            pickle.dump(model, file)
+        with open(os.path.join(savepath, filename[1]), 'wb') as file:
+            pickle.dump(encoder, file)
+        with open(os.path.join(savepath, filename[2]), 'wb') as file:
+            pickle.dump(lb, file)
         logger.info(f"Model saved to disk: {savepath}")
-    
-    
+
     # evaluate trained model on test set
     preds = inference(model, X_test)
     precision, recall, fbeta = compute_model_metrics(y_test, preds)
-    
+
     logger.info(f"Classification target labels: {list(lb.classes_)}")
     logger.info(
         f"precision:{precision:.3f}, recall:{recall:.3f}, fbeta:{fbeta:.3f}")
-    
+
     cm = compute_confusion_matrix(y_test, preds, labels=list(lb.classes_))
-    
+
     logger.info(f"Confusion matrix:\n{cm}")
-    
+
     # Compute performance on slices for categorical features
     # save results in a new txt file
     slice_savepath = args.slicefile
     remove_if_exists(slice_savepath)
-    
-    # iterate through the categorical features and save results to log and txt file
+
+    # iterate through the categorical features and save results to log and txt
+    # file
     for feature in cat_features:
         performance_df = compute_slices(test, feature, y_test, preds)
-        performance_df.to_csv(slice_savepath,  mode='a', index=False)
+        performance_df.to_csv(slice_savepath, mode='a', index=False)
         logger.info(f"Performance on slice {feature}")
         logger.info(performance_df)
 
@@ -173,7 +180,7 @@ if __name__ == '__main__':
 
     computer_name = platform.node()
     script_name = "model_trainner"
-    loggPath = os.path.join(".","log")
+    loggPath = os.path.join(".", "log")
     if not os.path.isdir(loggPath):
         try:
             # mode forced due security
@@ -181,18 +188,19 @@ if __name__ == '__main__':
             os.mkdir(loggPath, mode=mode)
         except OSError as error:
             print(error)
-            exit(-1)
-    LogFileName = os.path.join(loggPath, 
+            sys.exit(-1)
+    LogFileName = os.path.join(loggPath,
                                computer_name + '-' + script_name + '.log')
     # Configure the logger
     logger = log.getLogger(script_name)  # Get Logger
     # Add the log message file handler to the logger
-    logHandler = log.handlers.RotatingFileHandler(LogFileName, 
-                                                  maxBytes=10485760, 
+    logHandler = log.handlers.RotatingFileHandler(LogFileName,
+                                                  maxBytes=10485760,
                                                   backupCount=10)
     # Logger Formater
-    logFormatter = log.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s: %(message)s',
-                                datefmt='%Y/%m/%d %H:%M:%S')
+    logFormatter = log.Formatter(
+        fmt='%(asctime)s - %(name)s - %(levelname)s: %(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S')
     logHandler.setFormatter(logFormatter)
     # Add handler to logger
     if 'logHandler' in globals():
